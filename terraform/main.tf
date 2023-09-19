bf42e26a727e7bae81eab5bb82cdb784a37276ac
Shani <shaniben5253@gmail.com>
2023-09-19 11:40:25 +0300

    dd

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,92 +15,26 @@ provider "aws" {
 
 
 
-
-
-vpc_id              = "vpc-XXXXXXXXX"
-Instance_type       = "t2.medium"
-minsize             = 1
-maxsize             = 2
-public_subnets     = ["subnet-XXXXXXXXXX", "subnet-XXXXXXXXX"] # Service Subnet
-elb_public_subnets = ["subnet-XXXXXXXXXX", "subnet-XXXXXXXXX"] # ELB Subnet
-tier = "WebServer"
-solution_stack_name= "64bit Amazon Linux 2 v3.2.0 running Python 3.8"
-
-
-
-
-
-
-
-# Create elastic beanstalk application
-
-resource "aws_elastic_beanstalk_application" "elasticapp" {
-  name = var.elasticapp
+resource "aws_elastic_beanstalk_application" "2048app" {
+  name        = "2048app-shani-daria"
+  description = "2048app-shani-daria"
 }
 
-# Create elastic beanstalk Environment
+resource "aws_elastic_beanstalk_environment" "2048appenv" {
+  name                = "2048app-shani-daria"
+  application         = aws_elastic_beanstalk_application.2048app.name
+  solution_stack_name = "64bit Amazon Linux Docker running on 2023/4.0.1"
 
-resource "aws_elastic_beanstalk_environment" "elastic_beanstalk" {
-  name                = 2048app
-  application         = 2048app
-  solution_stack_name = var.solution_stack_name
-  tier                = var.tier
 
   setting {
     namespace = "aws:ec2:vpc"
     name      = "VPCId"
-    value     = var.vpc_id
-  }
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     =  "aws-elasticbeanstalk-ec2-role"
-  }
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "AssociatePublicIpAddress"
-    value     =  "True"
+    value     = "vpc-0d37ae25adc984356"
   }
 
   setting {
     namespace = "aws:ec2:vpc"
     name      = "Subnets"
-    value     = join(",", var.public_subnets)
+    value     = "subnet-0eb0c47b5d27e872e"
   }
-  setting {
-    namespace = "aws:elasticbeanstalk:environment:process:default"
-    name      = "MatcherHTTPCode"
-    value     = "200"
-  }
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "LoadBalancerType"
-    value     = "application"
-  }
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t2.medium"
-  }
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "ELBScheme"
-    value     = "internet facing"
-  }
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MinSize"
-    value     = 1
-  }
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MaxSize"
-    value     = 2
-  }
-  setting {
-    namespace = "aws:elasticbeanstalk:healthreporting:system"
-    name      = "SystemType"
-    value     = "enhanced"
-  }
-
 }
