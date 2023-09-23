93bb88d88b3b40686cc80a7cbcdb1d04f7ec6d98
Shani <shaniben5253@gmail.com>
2023-09-23 09:22:42 +0300

    add README.md

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
-variable "elasticapp" {
-  default = "myapp"
-}
-variable "beanstalkappenv" {
-  default = "myenv"
-}
-variable "solution_stack_name" {
-  type = string
-}
+#variable "elasticapp" {
+#  default = "myapp"
+#}
+#variable "beanstalkappenv" {
+#  default = "myenv"
+#}
+#variable "solution_stack_name" {
+#  type = string
+#}
 #variable "tier" {
 #  type = string
 #}
